2ca7d52f399b901874f38766c40cf0d3ba99bd4a
Carson Baker <carson@carsonbaker.org>
2013-12-21 00:38:33 -0600

    Server starts on  if present.

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
   "log"
   "text/template"
   "net/http"
+  "os"
 )
 
 var addr = flag.String("addr", ":8080", "http service address")
@@ -15,11 +16,18 @@ func homeHandler(c http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
+
   flag.Parse()
   go h.run()
   http.HandleFunc("/", homeHandler)
   http.HandleFunc("/ws", wsHandler)
+
+  if os.Getenv("PORT") != "" {
+    *addr = ":"+os.Getenv("PORT")
+  }
+
   if err := http.ListenAndServe(*addr, nil); err != nil {
     log.Fatal("ListenAndServe:", err)
   }
+
 }
